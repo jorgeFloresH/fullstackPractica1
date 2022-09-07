8d47c3629cf1a8bcab20eddf3577a3b6cbcbe306
jorgeFloresH <floreshjf@gmail.com>
2022-09-06 21:21:08 -0400

    cargar datos a la nube

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
+const mongoose= require("mongoose");
 const app = require("./app");
+const Product= require("./models/Product");
+
 const port = process.env.PORT;
-app.listen(port,"127.0.0.1", () => {
+mongoose.connect(process.env.DATABASE,{}).then((con)=>{
+  console.log("Coneccted to mongo.");
+  // const p=new Product({
+  //     productName:"product 2",
+  //     price:10
+  // });
+  // p.save().then(()=>{
+  //   console.log("saved product");
+  // });
+});
+app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
+// app.listen(port,"127.0.0.1", () => {
+//   console.log(`App running on port ${port}`);
+// });
